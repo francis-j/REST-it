9b9616de8a332e72ff6c64358f86bee2bcb087bc
Francis Jones <Francis@MBP.local>
2017-09-12 02:40:05 +0100

    Added new REST controller

---
+++ rest-it/rest-it/RESTController.swift
@@ -0,0 +1,54 @@
+//
+//  RESTController.swift
+//  rest-it
+//
+//  Created by Francis Jones on 12/09/2017.
+//  Copyright © 2017 fj. All rights reserved.
+//
+
+import UIKit
+
+class RESTController: UIViewController {
+    
+    @IBOutlet weak var SelectMethodButton: UIButton!
+    @IBOutlet weak var AddHeadersButton: UIButton!
+    @IBOutlet weak var SendButton: UIButton!
+    @IBOutlet weak var UrlText: UITextField!
+    @IBOutlet weak var BodyText: UITextView!
+    @IBOutlet weak var HeadersTable: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func SelectMethodButtonClicked(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func AddHeadersButtonClicked(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func SendButtonClicked(_ sender: UIButton) {
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
